5c36ffb1aaaa006f0f0c021cf4a7f7c009c4c151
MackRoe <elaine.music@students.makeschool.com>
2020-06-17 09:22:58 -0500

    continue debug. edits to makesite.go, README.md, and template.tmpl

--- makesite.go
+++ makesite.go
@@ -1,12 +1,31 @@
 package main
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// unknown purpose of this string. it was in the starter code
+// >> CODE CRED: https://github.com/squeaky1273
 import (
 	"io/ioutil"
+    "html/template"
+    "flag"
+    "os"
+    "strings"
 )
 
-func main() {
+type content struct {
+	Content string
+}
 
+func main() {
+    filePtr := flag.String("file", "", "filename")
+	flag.Parse()
+	content := readFile(*filePtr)
+//     # command-line-arguments
+// ./makesite.go:20:21: too many arguments in call to readFile
+// 	have (string)
+// 	want ()
+
+	renderTemplate("template.tmpl", content)
+	writeTemplateToFile("template.tmpl", *filePtr)
 }
 
 func readFile() string {
@@ -18,11 +37,33 @@ func readFile() string {
 	return string(fileContents)
 }
 
-func renderTemplate()
-=======
-import "fmt"
 
-func main() {
-	fmt.Println("Hello, world!")
+// >>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
+// unknown what the purpose is of this string. it was in the starter code
+func renderTemplate(filename string, data string) {
+	c := content{Content: data}
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+
+	var err error
+	err = t.Execute(os.Stdout , c)
+	if err != nil {
+		panic(err)
+	}
 }
->>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
+
+func writeTemplateToFile(filename string, data string) {
+	c := content{Content: data}
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+
+	fileName := strings.Split(filename, ".")[0] + ".html"
+	f, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(f, c)
+	if err != nil {
+		panic(err)
+	}
+
+}
